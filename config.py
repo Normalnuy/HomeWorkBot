78f4a3e397123c1f99c4289eee4e9780cf7b819f
Normalnuy <130695594+Normalnuy@users.noreply.github.com>
2024-07-25 16:38:26 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+TOKEN = "TOKEN HERE" 
+ADMIN_ID = ADMIN ID HERE
+
+WELCOME_STICKER = 'CAACAgIAAxkBAAIf-mXOY_FcAZ9iL-i-CO-cBRqxpw_RAAKIFAAC3VSISGrmV4NGSWg4NAQ'
+
+# GROUPS
+first_fac_nine_group = ID TG CHAT MY GROUP
+
+allow_groups = [first_fac_nine_group]
+
+# SUBJECTS
+eco_law = 'eco_law'
+crim_law = 'crim_law'
+crim_proc = 'crim_proc'
+now_kz = 'now_kz'
+law_es = 'law_es'
+civ_law = 'civ_law'
+civ_proc = 'civ_proc'
+po_prob = 'po_prob'
+
+
+# MAJNOR SUBJECTS
+first_majnor_subs = [eco_law, crim_law, crim_proc, now_kz, law_es, civ_law, civ_proc]
+second_majnor_subs = [eco_law, crim_law, crim_proc, po_prob, law_es, civ_law, civ_proc]
